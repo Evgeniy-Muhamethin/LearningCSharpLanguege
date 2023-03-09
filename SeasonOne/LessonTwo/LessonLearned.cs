cc2f6d55a862cc2cbe167432bceecba5d64992d8
Evgeniy-Muhamethin <John090918@gmail.com>
2023-03-09 13:33:26 +0300

    Half lesson is learned

---
+++ SeasonOne/LessonTwo/LessonLearned.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace LessonTwo
+{
+	enum Turn
+	{
+		turnOn,
+		turnOff,
+		turnNot,
+	};
+
+	internal class LessonLearned
+	{
+		public int TernarOperators(int a, int b)
+		{
+			int result = a > b ? 1 : -1;
+			return result;
+		}
+
+		public void SwichAndCase(bool flag)
+		{
+			switch (flag) 
+			{ 
+				case true:
+					int result1 = 10 + 12;
+                    Console.WriteLine(result1);
+					break;
+				case false:
+					int result2 = 56 + 10;
+					Console.WriteLine(result2);
+					break;
+			}
+		}
+
+		public void EnumOperators(Turn turn)
+		{
+			switch(turn) 
+			{ 
+				case Turn.turnOn:
+					Console.WriteLine("Flight on");
+					break;
+				case Turn.turnOff:
+					Console.WriteLine("Flight off");
+					break;
+				case Turn.turnNot:
+					Console.WriteLine("Flight not");
+					break;
+			}
+		}
+	}
+}
---
+++ SeasonOne/LessonTwo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLibrary;
+
+namespace LessonTwo
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			LibraryClass libraryClass = new LibraryClass();
+			LessonLearned lessonLearned = new LessonLearned();
+
+			#region Operators 
+			//int result = lessonLearned.TernarOperators(4,3);
+
+			//Turn turn;
+
+			//if (result == -1)
+			//{
+			//	turn = Turn.turnOff;
+			//	lessonLearned.EnumOperators(turn);
+			//}
+			//else if (result == 1)
+			//{
+			//	turn = Turn.turnOn;
+			//	lessonLearned.EnumOperators(turn);
+			//}
+			//else
+			//{
+			//	turn = Turn.turnNot;
+			//	lessonLearned.EnumOperators(turn);
+			//}
+			#endregion
+
+
+
+			libraryClass.Pause();
+
+		}
+	}
+}
