d2e5b1ddaa1dc13aaadeabe26e1065bd796c31e7
Evgeniy-Muhamethin <John090918@gmail.com>
2023-03-15 15:37:17 +0300

    New lesson was starting to learn

---
+++ SeasonOne/LessonSix/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonSix
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			LessonLearned lessonLearned = new LessonLearned();
+		}
+	}
+}
