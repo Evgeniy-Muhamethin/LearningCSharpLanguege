8b4d2e0d136a025e19d836ae3a7c294fcb011174
Evgeniy-Muhamethin <John090918@gmail.com>
2023-06-26 13:00:35 +0300

    Start Learning

--- SeasonOne/LessonOne/Program.cs
+++ SeasonOne/LessonOne/Program.cs
@@ -5,8 +5,14 @@ namespace LessonOne
 {
 	internal class Program
 	{
+		/// <summary>
+		/// Коментарии описывающие класс или метод
+		/// </summary>
+		/// <param name="args">Место для описания параметров метода</param>
 		static void Main(string[] args)
 		{
+			#region old programms code
+			/*
 			HomeWork homeWork = new HomeWork();
 			LessonIsLearned lessonIsLearned = new LessonIsLearned();
 			LibraryClass libraryClass = new LibraryClass();
@@ -15,6 +21,10 @@ namespace LessonOne
 				"Racoon city", 35, 15, ConsoleColor.Green);
 
 			libraryClass.Pause();
-        }
+			*/
+			#endregion
+			Console.WriteLine("Hello world!!");
+			Console.ReadLine();
+		}
 	}
 }
